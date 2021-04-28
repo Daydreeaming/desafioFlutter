f0459defbe8f14656da4c830d9c3b6e636c0bcb6
Lucas Dantas <lucascamposdantas@hotmail.com>
2021-04-28 20:55:52 -0300

    Implementação da API e parse. Model, controller e criação da classe Movie

--- lib/movie_controller.dart
+++ lib/movie_controller.dart
@@ -1,2 +1,12 @@
 import 'package:desafioflutter/movie_model.dart';
 import 'package:desafioflutter/movie.dart';
+
+class MovieController {
+  final model = MovieModel();
+
+  Future<Movie> get movie => model.movie;
+
+  loadMovie() {
+    model.fetchMovies();
+  }
+}
